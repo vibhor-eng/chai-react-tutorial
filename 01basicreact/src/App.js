c1cceff0fdc1c12f0d92446966aaf6aaf06b3371
vibhor-eng <mayurty88@gmail.com>
2025-03-27 18:02:03 +0530

    lecture 3

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
 
-
+import Chai from "./Chai";
 function App() {
   return (
+    <>
+    <Chai />
     <h1>Chai aur code React NPX | Vibhor</h1>
+    </>
   );
 }
 
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+function Chai(){
+
+    return (
+
+        <h2>chai in react</h2>
+    )
+
+}
+
+export default Chai
